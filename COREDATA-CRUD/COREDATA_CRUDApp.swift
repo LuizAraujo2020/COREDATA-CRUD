42a816cbed400abc5c87bb02243828383ac3186e
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-01-09 11:45:53 -0300

    Initial Commit

---
+++ COREDATA-CRUD/COREDATA_CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  COREDATA_CRUDApp.swift
+//  COREDATA-CRUD
+//
+//  Created by Luiz Araujo on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct COREDATA_CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
